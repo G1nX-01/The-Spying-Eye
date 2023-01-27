480286d654cc08c93c750ea2b4854eeb2254f57a
G1nX-01 <81038333+G1nX-01@users.noreply.github.com>
2023-01-27 13:36:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ xhr.onload = function() {
     var latitude = response.latitude;
     var longitude = response.longitude;
     var currency = response.currency_name;
-    document.getElementById("doxtext").innerHTML = "You live in " + country_name + "." + " Your latitude is " + latitude + " and your longitude is " + longitude + "." + " Your region is " + region + " and you live in " + city + "." + ' Your currency is ' + '"' + currency + '".' + " Your IP is: " + dox + "."
+	var provider = response.org;
+	var eu = response.in_eu;
+	if (!eu) {eu = "You don't live in the European Union."} else {eu = "You live in the European Union."}
+    document.getElementById("doxtext").innerHTML = "You live in " + country_name + "." + " Your latitude is " + latitude + " and your longitude is " + longitude + "." + " Your region is " + region + " and you live in " + city + "." + ' Your currency is ' + '"' + currency + '".' + " Your IP is: " + dox + '. Your internet provider (isp) is "' + provider + '". ' + eu
   }
 };
 xhr.send();
